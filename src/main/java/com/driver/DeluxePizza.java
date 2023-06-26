1d1e4c85efdcfa8e65f1005ab389742a456a15c0
Ayush Singh <ayushsingh2442@gmail.com>
2023-06-26 14:14:57 +0530

    first

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -7,10 +7,10 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
-        if(isVeg){
+
            this.addExtraCheese();
            this.addExtraToppings();
-        }
+
     }
 
 }
